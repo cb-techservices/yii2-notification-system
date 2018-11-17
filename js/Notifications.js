366043517880181adde1fb3c235e1aeb61a37f8d
carlb <burnstei@uoregon.edu>
2018-11-16 20:13:47 -0800

    -Updated Notifications.js -Added notification list view rendering. renderRow() method builds the list. Custom template can be provided with the listItemTemplate option. -MarkAsRead is working and hides the notification from the list view -Also added data-keepOpenOnClick which allows items within a notification body to be clickable if they have data-keepOpenOnClick as a parameter.

--- js/Notifications.js
+++ js/Notifications.js
@@ -5,10 +5,10 @@
 //		function notify(){ //This syntax makes the function callable only from within the class.
     	this.notify = function(notification){ //This syntax makes the function callable by the object (this)
     		toastr.options.closeButton = true;
-//    		toastr.options.timeOut = this.opts.delay; // How long the toast will display without user interaction
-//    		toastr.options.extendedTimeOut = 30; // How long the toast will display after a user hovers over it
-    		toastr.options.timeOut = 0;
-    		toastr.options.extendedTimeOut = 0;
+    		toastr.options.timeOut = this.opts.delay; // How long the toast will display without user interaction
+    		toastr.options.extendedTimeOut = this.opts.delay; // How long the toast will display after a user hovers over it
+//    		toastr.options.timeOut = 0;
+//    		toastr.options.extendedTimeOut = 0;
     		
     		if(notification.type == 'default'){
     			toastr.info(notification.body, notification.title);
@@ -34,12 +34,12 @@
     				window.location = notification.url;
     			}
     			//mark as read
-    			markAsRead(notification);
+    			markAsRead(notification.id);
     		}
     		toastr.options.onCloseClick = function() { 
     			console.log('close button clicked');
     			//mark as read
-    			markAsRead(notification);
+    			markAsRead(notification.id);
     		}
     	}
     	
@@ -64,15 +64,18 @@
             deleteAllSelector: null,
             listSelector: null,
             listItemTemplate:
-                '<div class="row">' +
+                '<div class="notificationRow" id="notification_{id}">' +
                     '<div class="col-xs-10">' +
                         '<div class="title">{title}</div>' +
-                        '<div class="description">{description}</div>' +
-                        '<div class="timeago">{timeago}</div>' +
+                        '<div class="body">{body}</div>' +
+                        
                     '</div>' +
                     '<div class="col-xs-2">' +
-                        '<div class="actions pull-right">{seen}{delete}</div>' +
+                        '<div class="actions pull-right">{read}{delete}</div>' +
+                        '<div class="timeago pull-right">{timeago}</div>' +
                     '</div>' +
+                    '<div class="clearfix"></div>' + 
+                    '<hr/>' +
                 '</div>',
             listItemBeforeRender: function (elem) {
                 return elem;
@@ -93,25 +96,32 @@
         		.complete(function(json){
         			console.log(json.responseJSON);
         			var notifications = json.responseJSON;
+        			var rows = "";
         			for(i in notifications){
         				var notification = notifications[i];
         				if(notification.flashed == 0){
         					notify(notification);
         				}
+        				rows += renderRow(notification);
+        			}
+        			if(opts.listSelector != null && opts.listSelector != ""){
+        				$(opts.listSelector).empty().append(rows);
         			}
         		});
         }
         
-        this.markAsRead = function(notification){
-        		console.log(notification);
+        this.markAsRead = function(id){
+        		console.log(id);
         		$.ajax({
         			url: this.opts.markAsReadUrl,
         			method: "GET",
-        			data: {id:notification.id},
+        			data: {id:id},
         			dataType: "json"
         		})
         		.complete(function(json){
-        			
+        			if($("#notification_"+id).length){
+        				$("#notification_"+id).slideUp();
+        			}
         		});
         }
         
@@ -139,7 +149,25 @@
 	    		});
         }
         
+        this.renderRow = function(notification){
+        		var html = "<div class=''>" + self.opts.listItemTemplate + "</div>";
+        		html = html.replace(/\{id}/g, notification.id);
+        		html = html.replace(/\{title}/g, notification.title);
+        		html = html.replace(/\{body}/g, notification.body);
+        		html = html.replace(/\{read}/g, '<span onclick="markAsRead(' + notification.id + ');" class="notification-seen glyphicon glyphicon-ok" data-keepOpenOnClick></span>');
+            html = html.replace(/\{delete}/g, '<span onclick="" class="notification-delete glyphicon glyphicon-remove" data-keepOpenOnClick></span>');
+//            html = html.replace(/\{timeago}/g, '<span class="notification-timeago">' + notification.timeago +'</span>');
+            
+            return html;
+        }
+        
 //        notify();
+        
+        $(document).ready(function(){
+        		$(document).delegate("ul.dropdown-menu [data-keepOpenOnClick]", "click", function(e) {
+        			e.stopPropagation();
+        		});
+        });
         
         return this;
     });
